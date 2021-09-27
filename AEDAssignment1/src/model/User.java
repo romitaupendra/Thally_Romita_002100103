e138cd637120367eb07d3cda3a0079667dc7a5ed
romitaupendra <thally.r@northeastern.edu>
2021-09-26 23:40:32 -0400

    first commit

---
+++ AEDAssignment1/src/model/User.java
@@ -0,0 +1,180 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+
+/**
+ *
+ * @author 17814
+ */
+public class User {
+    
+    private String fName;
+    private String mailAddress;
+    private String bDate;
+    private String mobNum;
+    private String faxNum;
+    private String emailId;
+    private String socSecNum;
+    private String medRecNum;
+    private String healthPlanBenNum;
+    private String banAccNum;
+    private String certLicNum;
+    private String vehType;
+    private String vehNumber;
+    private String devIdNum;
+    private String linkedinId;
+    private String ipAddress;
+    private String bioId;
+    
+    
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getbDate() {
+        return bDate;
+    }
+
+    public void setbDate(String bDate) {
+        this.bDate = bDate;
+    }
+
+   
+
+    public String getMobNum() {
+        return mobNum;
+    }
+
+    public void setMobNum(String mobNum) {
+        this.mobNum = mobNum;
+    }
+
+    public String getFaxNum() {
+        return faxNum;
+    }
+
+    public void setFaxNum(String faxNum) {
+        this.faxNum = faxNum;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getSocSecNum() {
+        return socSecNum;
+    }
+
+    public void setSocSecNum(String socSecNum) {
+        this.socSecNum = socSecNum;
+    }
+
+    public String getMedRecNum() {
+        return medRecNum;
+    }
+
+    public void setMedRecNum(String medRecNum) {
+        this.medRecNum = medRecNum;
+    }
+
+    public String getHealthPlanBenNum() {
+        return healthPlanBenNum;
+    }
+
+    public void setHealthPlanBenNum(String healthPlanBenNum) {
+        this.healthPlanBenNum = healthPlanBenNum;
+    }
+
+    public String getBanAccNum() {
+        return banAccNum;
+    }
+
+    public void setBanAccNum(String banAccNum) {
+        this.banAccNum = banAccNum;
+    }
+
+    public String getCertLicNum() {
+        return certLicNum;
+    }
+
+    public void setCertLicNum(String certLicNum) {
+        this.certLicNum = certLicNum;
+    }
+
+    public String getVehType() {
+        return vehType;
+    }
+
+    public void setVehType(String vehType) {
+        this.vehType = vehType;
+    }
+
+    public String getVehNumber() {
+        return vehNumber;
+    }
+
+    public void setVehNumber(String vehNumber) {
+        this.vehNumber = vehNumber;
+    }
+
+    public String getDevIdNum() {
+        return devIdNum;
+    }
+
+    public void setDevIdNum(String devIdNum) {
+        this.devIdNum = devIdNum;
+    }
+
+    public String getLinkedinId() {
+        return linkedinId;
+    }
+
+    public void setLinkedinId(String linkedinId) {
+        this.linkedinId = linkedinId;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBioId() {
+        return bioId;
+    }
+
+    public void setBioId(String bioId) {
+        this.bioId = bioId;
+    }
+
+    
+   
+    
+    
+    
+    
+    
+}
